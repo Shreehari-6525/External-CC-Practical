cbd81e106c46496fb294c4077a26b4e8aec4c3f8
Shreehari-6525 <148474675+Shreehari-6525@users.noreply.github.com>
2026-05-07 14:39:36 +0530

    Create Sorting.py

---
+++ Sorting.py
@@ -0,0 +1,10 @@
+print("Sorting of Number Using Google App Engine")
+
+def Sort_data(arr):
+    sorted_array = arr.sort()
+    print("Sorted Array: ", sorted_array)
+          
+
+if __name__ == "__main__":
+    arr = [23,4,3,32,1,25,78,46]
+    sort_data(arr)
